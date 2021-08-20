46faf40d56a063119bc5297510b8e9521fab1d79
Jsieler <joshuasieler@Joshs-Air.home>
2021-08-20 10:43:49 -0600

    had to move ReactionSchema model above ThoughtSchema model, tested get all users route it works

--- models/Thought.js
+++ models/Thought.js
@@ -1,33 +1,5 @@
 const { Schema, model, Types } = require('mongoose')
 
-const ThoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        required: 'Thought is required',
-        minlength: 1,
-        maxlength: 280
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
-    },
-    username: {
-        type: String,
-        required: 'Username is Required'
-    },
-    reactions: [ReactionSchema]
-},
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        // prevents virtuals from creating duplicate of _id as 'id'
-        id: false
-    }
-);
-
 const ReactionSchema = new Schema({
     reactionId: {
         type: Schema.Types.ObjectId,
@@ -53,6 +25,34 @@ const ReactionSchema = new Schema({
             getters: true
         }
     });
+
+    const ThoughtSchema = new Schema({
+        thoughtText: {
+            type: String,
+            required: 'Thought is required',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: 'Username is Required'
+        },
+        reactions: [ReactionSchema]
+    },
+        {
+            toJSON: {
+                virtuals: true,
+                getters: true
+            },
+            // prevents virtuals from creating duplicate of _id as 'id'
+            id: false
+        }
+    );
 
     ThoughtSchema.virtual('reactionCount').get(function() {
         return this.reactions.length;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const db = require('./models');
+const db = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,5 +16,21 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-net-api'
 });
 
 mongoose.set('debug', true);
+
+// Retrieve all users
+app.get('/users', (req, res) => {
+  db.User.find({})
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+
+
+
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
